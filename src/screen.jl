4597abcafbc17284fd9a98db48de3af89fc72805
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-12-23 18:54:24 -0600

    improve display stability by resetting to beginning of lines

--- src/screen.jl
+++ src/screen.jl
@@ -28,13 +28,13 @@ function render(io::IO, s::Screen)
         for x in axes(s, 2)
             if s[y, x] != s.display_state[y, x]
                 new_cursor = (y, x)
-                move_cursor(iob, new_cursor .- cursor)
+                move_cursor(iob, new_cursor, cursor)
                 cursor = new_cursor .+ (0, 1)
                 write(iob, s[y, x])
             end
         end
     end
-    move_cursor(iob, (size(s, 1), 1) .- cursor)
+    move_cursor(iob, (size(s, 1), 1), cursor)
     copyto!(s.display_state, s.buffer)
     if s.bell[]
         write(iob, '\a')
@@ -45,16 +45,22 @@ function render(io::IO, s::Screen)
 end
 
 str(n) = n == 1 ? "" : "$n"
-function move_cursor(io::IO, Δ)
+function move_cursor(io::IO, new, old)
     # TODO: optimize
-    if Δ[1] > 0
-        print(io, "\e[", str(Δ[1]), "B")
-    elseif Δ[1] < 0
-        print(io, "\e[", str(-Δ[1]), "A")
+    Δy = new[1] - old[1]
+    x = if Δy > 0
+        print(io, "\e[", str(Δy), "E")
+        1
+    elseif Δy < 0
+        print(io, "\e[", str(-Δy), "F")
+        1
+    else
+        old[2]
     end
-    if Δ[2] > 0
-        print(io, "\e[", str(Δ[2]), "C")
-    elseif Δ[2] < 0
-        print(io, "\e[", str(-Δ[2]), "D")
+    Δx = new[2] - x
+    if Δx > 0
+        print(io, "\e[", str(Δx), "C")
+    elseif Δx < 0
+        print(io, "\e[", str(-Δx), "D")
     end
 end
