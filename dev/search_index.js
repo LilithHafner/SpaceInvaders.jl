af7d9e11ebc4e50be64b2944ace62ed5b7fec833
Documenter.jl <documenter@juliadocs.github.io>
2023-12-24 01:45:40 +0000

    build based on 5fb428f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpaceInvaders","category":"page"},{"location":"#SpaceInvaders","page":"Home","title":"SpaceInvaders","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpaceInvaders.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpaceInvaders]","category":"page"}]
+}
