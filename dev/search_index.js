9f87769b16f69420ea59da764563325ab8ebf837
Documenter.jl <documenter@juliadocs.github.io>
2025-11-25 16:06:59 +0000

    build based on ee648c8

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SpaceInvaders","page":"Home","title":"SpaceInvaders","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpaceInvaders.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#SpaceInvaders.main-Tuple{}","page":"Home","title":"SpaceInvaders.main","text":"main()\n\nLaunch space invaders.\n\nRequires a terminal with support for ANSI escape codes for cursor movement.\n\n\n\n\n\n","category":"method"}]
+[{"location":"#SpaceInvaders","page":"Home","title":"SpaceInvaders","text":"Documentation for SpaceInvaders.\n\n","category":"section"},{"location":"#SpaceInvaders.main-Tuple{}","page":"Home","title":"SpaceInvaders.main","text":"main()\n\nLaunch space invaders.\n\nRequires a terminal with support for ANSI escape codes for cursor movement.\n\n\n\n\n\n","category":"method"}]
 }
